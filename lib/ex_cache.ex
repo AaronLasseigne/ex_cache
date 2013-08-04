ba56287911f6e24e0c8ea8b543eb514014ce9760
Aaron Lasseigne <aaron.lasseigne@gmail.com>
2013-08-03 22:43:18 -0500

    dry up the server calls

--- lib/ex_cache.ex
+++ lib/ex_cache.ex
@@ -6,14 +6,18 @@ defmodule ExCache do
   end
 
   def read(key) do
-    :gen_server.call(:ex_cache, {:read, key})
+    call({:read, key})
   end
 
   def write(key, value) do
-    :gen_server.call(:ex_cache, {:write, key, value})
+    call({:write, key, value})
   end
 
   def exist?(key) do
-    :gen_server.call(:ex_cache, {:exist?, key})
+    call({:exist?, key})
+  end
+
+  defp call(args) do
+    :gen_server.call(:ex_cache, args)
   end
 end
